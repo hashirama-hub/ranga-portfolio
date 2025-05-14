cfac10fc58b5bc8ce0cc79e55246a571d2c1b5e1
Tuan Linh <81902717+hashirama-hub@users.noreply.github.com>
2025-05-14 18:58:25 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Xóa dòng này để cho phép SSR và API Routes hoạt động
+  // output: 'export',
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true
+  },
+  // Bạn có thể thêm các cấu hình khác của Next.js tại đây nếu cần
 };
 
 module.exports = nextConfig;
